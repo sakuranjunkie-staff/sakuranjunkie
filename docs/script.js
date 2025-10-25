08ed9992f1c81c159e715166e93a9aa11f7029b9
sakuranjunkie-staff <239869839+sakuranjunkie-staff@users.noreply.github.com>
2025-10-26 02:45:16 +0900

    Update script.js
    Signed-off-by: sakuranjunkie-staff <239869839+sakuranjunkie-staff@users.noreply.github.com>

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,8 @@
-$(function() {
+$(document).ready(function() { // DOM準備完了を保証
     $('.hamburger').click(function() {
+        // メニューの開閉状態を切り替える
         $('.menu').toggleClass('open');
+        // ハンバーガーボタンのアクティブクラスを切り替えて三本線をバツにする
         $(this).toggleClass('active');
     });
 });
